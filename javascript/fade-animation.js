2680d0addd0c3cd25ceb85b75a094be5e13a9956
Vladislav <70971642+YBHPG@users.noreply.github.com>
2022-05-12 10:24:59 +0300

    Added modular system to CSS

--- javascript/fade-animation.js
+++ javascript/fade-animation.js
@@ -7,16 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
 var workButton = document.getElementById("fade-out_work-button");
 var aboutButton = document.getElementById("fade-out_about-button");
 
-/* window.onclick = function (event) {
-    if (event.target == myButton) {
-        window.setTimeout(function () {
-            document.body.classList.remove("fade");
-            document.body.classList.add("fadeOut");
-            console.log(event.target);
-        }, 500);
-    }
-} */
-
 function fadeOutWork() {
     document.body.classList.remove("fade");
     document.body.classList.add("fadeOut");
