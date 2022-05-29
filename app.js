fb7f73d99b888b2a39437182c2e5f660953470ed
Vladislav <70971642+YBHPG@users.noreply.github.com>
2022-05-29 15:49:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 import express from 'express';
 import path from 'path';
 import fs from 'fs';
+import http from 'http';
 import https from 'https';
 
 const __dirname = path.resolve();
-const adress = '194.169.160.214';
-const port = 80;
+const adress = '127.0.0.1';
+const httpPort = 8080;
+const httpsPort = 8443;
 
 const cert = fs.readFileSync('./ssl/certificate.crt');
 const ca = fs.readFileSync('./ssl/ca_bundle.crt');
 const privateKey = fs.readFileSync('./ssl/private.key');
+
+const app = express();
 
 const httpOptions = {
     cert: cert,
     ca: ca,
     key: privateKey
 };
-
-const app = express();
+const httpServer = http.createServer(app);
 const httpsServer = https.createServer(httpOptions, app);
 
-app.use('/static', express.static(__dirname + '/static'))
+app.use('/static', express.static(__dirname + '/static'));
+app.use((req, res, next) => {
+    if (req.protocol === 'http') {
+        res.redirect(301, `https://${adress}:${httpsPort}`);
+    } else {
+        next();
+    }
+});
 
 /* ==================================================*/
 /* GETTERS */
@@ -64,6 +74,9 @@ app.get('/.well-known/4ABA000B996255A60A39992EA1D7CC5C.txt', (req, res) => {
 /* ==================================================*/
 /* START */
 
-httpsServer.listen(port, adress, () => {
-    console.log(`Server is running at https://${adress}:${port}`);
+httpServer.listen(httpPort, adress, () => {
+    console.log(`Server is running at http://${adress}:${httpPort}`);
+});
+httpsServer.listen(httpsPort, adress, () => {
+    console.log(`Server is running at https://${adress}:${httpsPort}`);
 });
