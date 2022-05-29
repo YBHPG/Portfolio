66f8926c94e5d528b740bfffc0be6c12df212184
Vladislav <70971642+YBHPG@users.noreply.github.com>
2022-05-29 14:09:38 +0300

    Added SSL certificates to the server

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
+import https from 'https';
 
 const __dirname = path.resolve();
 const adress = '194.169.160.214';
 const port = 80;
 
+const cert = fs.readFileSync('./ssl/certificate.crt');
+const ca = fs.readFileSync('./ssl/ca_bundle.crt');
+const privateKey = fs.readFileSync('./ssl/private.key');
+
+const httpOptions = {
+    cert: cert,
+    ca: ca,
+    key: privateKey
+};
+
 const app = express();
+const httpsServer = https.createServer(httpOptions, app);
+
 app.use('/static', express.static(__dirname + '/static'))
 
 /* ==================================================*/
@@ -40,16 +54,16 @@ app.get('/en/about', (req, res) => {
 });
 
 app.get('/.well-known/pki-validation/4ABA000B996255A60A39992EA1D7CC5C.txt', (req, res) => {
-    res.sendFile(__dirname + '/static/txt/4ABA000B996255A60A39992EA1D7CC5C.txt')
+    res.sendFile(__dirname + '/static/txt/4ABA000B996255A60A39992EA1D7CC5C.txt');
 });
 
 app.get('/.well-known/4ABA000B996255A60A39992EA1D7CC5C.txt', (req, res) => {
-	res.sendFile(__dirname + '/static/txt/4ABA000B996255A60A39992EA1D7CC5C.txt');
+    res.sendFile(__dirname + '/static/txt/4ABA000B996255A60A39992EA1D7CC5C.txt');
 });
 
 /* ==================================================*/
 /* START */
 
-app.listen(port, adress, () => {
-    console.log(`Server is running at http://${adress}:${port}`);
+httpsServer.listen(port, adress, () => {
+    console.log(`Server is running at https://${adress}:${port}`);
 });
