f8d4f472427c50bd63b8a0263623269aa222d1e4
Vladislav <70971642+YBHPG@users.noreply.github.com>
2022-05-29 18:08:13 +0300

    reverting ssl

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
 import express from 'express';
 import path from 'path';
-import fs from 'fs';
-import http from 'http';
-import https from 'https';
 
 const __dirname = path.resolve();
-const adress = '127.0.0.1';
-const httpPort = 8080;
-const httpsPort = 8443;
-
-const cert = fs.readFileSync('./ssl/certificate.crt');
-const ca = fs.readFileSync('./ssl/ca_bundle.crt');
-const privateKey = fs.readFileSync('./ssl/private.key');
+const adress = '0.0.0.0';
+const port = 8080;
 
 const app = express();
-
-const httpOptions = {
-    cert: cert,
-    ca: ca,
-    key: privateKey
-};
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(httpOptions, app);
-
-app.use('/static', express.static(__dirname + '/static'));
-app.use((req, res, next) => {
-    if (req.protocol === 'http') {
-        res.redirect(301, `https://${adress}:${httpsPort}`);
-    } else {
-        next();
-    }
-});
+app.use('/static', express.static(__dirname + '/static'))
 
 /* ==================================================*/
 /* GETTERS */
@@ -64,7 +40,7 @@ app.get('/en/about', (req, res) => {
 });
 
 app.get('/.well-known/pki-validation/4ABA000B996255A60A39992EA1D7CC5C.txt', (req, res) => {
-    res.sendFile(__dirname + '/static/txt/4ABA000B996255A60A39992EA1D7CC5C.txt');
+    res.sendFile(__dirname + '/static/txt/4ABA000B996255A60A39992EA1D7CC5C.txt')
 });
 
 app.get('/.well-known/4ABA000B996255A60A39992EA1D7CC5C.txt', (req, res) => {
@@ -74,9 +50,6 @@ app.get('/.well-known/4ABA000B996255A60A39992EA1D7CC5C.txt', (req, res) => {
 /* ==================================================*/
 /* START */
 
-httpServer.listen(httpPort, adress, () => {
-    console.log(`Server is running at http://${adress}:${httpPort}`);
-});
-httpsServer.listen(httpsPort, adress, () => {
-    console.log(`Server is running at https://${adress}:${httpsPort}`);
+app.listen(port, adress, () => {
+    console.log(`Server is running at http://${adress}:${port}`);
 });
