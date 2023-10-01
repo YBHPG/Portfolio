d706ce249144f13a5401395815898a0326ad1cde
Vladislav <70971642+YBHPG@users.noreply.github.com>
2023-10-01 23:49:31 +0300

    Отключил метрику

--- static/javascript/languageDetect.js
+++ static/javascript/languageDetect.js
@@ -1,11 +1,16 @@
 function pageLoad() {
   var userLang = navigator.language || navigator.userLanguage;
+  print("navigator")
 
   if (document.cookie.length === 0) {
+    print("cookie create")
     document.cookie = `userLang=${userLang}; max-age=${365 * 24 * 60 * 60}; path=/; secure`;
+    print("cookie")
     window.location.href = "./work";
+    print("return")
     return;
   } else {
+    print("return")
     window.location.href = "./work";
     return;
   }
