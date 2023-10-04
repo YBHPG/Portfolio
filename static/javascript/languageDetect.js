f4dae342ef1364300261695275a122cab57b18f1
Vladislav Ledashchev <70971642+YBHPG@users.noreply.github.com>
2023-10-04 23:24:14 +0300

    Сменил print на console.log......

--- static/javascript/languageDetect.js
+++ static/javascript/languageDetect.js
@@ -1,17 +1,17 @@
 function pageLoad() {
   var userLang = navigator.language || navigator.userLanguage;
-  print("navigator")
+  console.log("navigator")
 
   if (document.cookie.length === 0) {
-    print("cookie create")
+    console.log("cookie create")
     document.cookie = `userLang=${userLang}; max-age=${365 * 24 * 60 * 60}; path=/; secure`;
-    print("cookie")
+    console.log("cookie")
     window.location.href = "./work";
-    print("return")
     return;
   } else {
-    print("return")
+    console.log("return")
     window.location.href = "./work";
+    console.log("return 200 OK")
     return;
   }
 
